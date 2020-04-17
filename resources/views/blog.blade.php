142ac35b778cf479256cbce7227067b51c21b9a0
Swami Viprananda <viprananda@rkmm.org>
2020-04-17 10:57:11 +0530

    Update blog.blade.php

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -82,20 +82,27 @@
                 </div>
             @endif
 
+
             
+<div class="container">
+            <div class="card text-center">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+</div>
 
 
 
-                <div class="links">
-                    <a href="/aboutus">About Us</a>
-                    <a href="/poems">Poems</a>
-                    <a href="/quotes">Quotes</a>
-                    <a href="/news">News</a>
-                    <a href="/forum">Forum</a>
-                    <a href="/blog">Blog</a>                    
-                    <a href="/contact">Contact Us</a>
-                    <a href="/sitemap">Sitemap</a>
-                </div>
+                
             </div>
         </div>
     </body>
