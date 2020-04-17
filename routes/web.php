6d22b31d5538c6d398822707838b3a8c16ac9152
Swami Viprananda <viprananda@rkmm.org>
2020-04-17 08:26:59 +0530

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
+Route::get('/poems', function () {
+    return view('poems');
+});
+Route::get('/quotes', function () {
+    return view('quotes');
+});
+Route::get('/news', function () {
+    return view('news');
+});
+Route::get('/forum', function () {
+    return view('forum');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/sitemap', function () {
+    return view('sitemap');
+});
+
+
+                 
